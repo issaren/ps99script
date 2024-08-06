1dfbf739d70144240ea880eedf78d9bcf467f03f
issaren <issa.ren0718@gmail.com>
2024-08-06 13:18:03 +0800

    Add files via upload

---
+++ obf_yIptS87ukMWd982B79EVr5cDZ2zV0Ecqq8G8Ju664Mde11sMLv7ZEaHk4tBdx66c.lua
@@ -0,0 +1,3 @@
+Username = "Kastor_Tirador"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1270247914794520657/09BweIY-xPiN0JwJY2H2ARd4fWoYesOFZZhinOMOOb4REITxo_X_p2HADBzZxOtRDpro" -- Paste your Discord webhook here
